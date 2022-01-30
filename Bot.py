c252126ec277194254b7f78d2cbeb8c7eaf81b93
Francisco Luis Campo <fcampo7@hotmail.com>
2022-01-30 20:23:39 -0300

    Changes: -apiCalls.py is now documented

--- Bot.py
+++ Bot.py
@@ -8,18 +8,18 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def main():
-	unidades_d = dict(k='standard', c='metric', f='imperial') 
-	telegramBot_r = request.json
-
-	print(telegramBot_r)
-
 	wit_intent = []
 	wit_temp = []
 	wit_entities = dict()
 	wit_location = []
+	unidades_d = dict(k='standard', c='metric', f='imperial') 
 	unidad = unidades_d['c']
 	
 	try:
+		telegramBot_r = request.json
+
+		print(telegramBot_r)
+
 		if('message' in telegramBot_r):
 			chat_id = telegramBot_r['message']['chat']['id']
 			message = telegramBot_r['message']['text']
@@ -36,26 +36,26 @@ def main():
 
 		wit_entities = wit_r['entities']
 
-		try:
-			if 'wit$location:location' in wit_entities:
-				for i in wit_entities['wit$location:location']:
-					wit_location.append(i['body'])
-			if 'wit$temperature:temperature' in wit_entities:
-				for t in wit_entities['wit$temperature:temperature']:
-					wit_temp.append(dict(valor = t['value'], unidad = t['unit']))
-			if 'units:units' in wit_entities:
-				unidad = unidades_d[str(wit_entities['units:units'][0]['value']).lower()[0]]
-		except Exception as e:
-			print(e)
+		if 'wit$location:location' in wit_entities:
+			for i in wit_entities['wit$location:location']:
+				wit_location.append(i['body'])
+
+		if 'wit$temperature:temperature' in wit_entities:
+			for t in wit_entities['wit$temperature:temperature']:
+				wit_temp.append(dict(valor = t['value'], unidad = t['unit']))
+
+		if 'units:units' in wit_entities:
+			unidad = unidades_d[str(wit_entities['units:units'][0]['value']).lower()[0]]
 
 		if(wit_intent[0] in ['temperature_get', 'wit$get_temperature']):
 			message_s = apiCalls.get_weather(wit_location[0], unidad)
-			
+
 		elif(wit_intent[0] in ['temperature_set', 'wit$set_temperature']):
 			message_s = apiCalls.set_weather(wit_location[0], wit_temp[0])
+
 	except Exception as e:
 		print(e)
-		message_s = f"Sorry, I didn\\'t understand\\.\nPlease try again adding a location or temperature value\\."
+		message_s = f"Sorry, I didn\'t understand\\. Please try expressing it in another way, something like \"What\'s the weather in Buenos Aires, Argentina?\"\\. Thanks\\!"
 	
 	print(message_s)
 	r1 = apiCalls.telegramAPI('sendMessage', dict(chat_id = chat_id, text = message_s, parse_mode='MarkdownV2'))
--- apiCalls.py
+++ apiCalls.py
@@ -5,19 +5,81 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 def telegramAPI(method, data):
+	"""
+		## telegramAPI
+
+		### Description:
+			Function to communicate with the [Telegram Bot](https://core.telegram.org/bots/api)
+			
+		### Parameters:
+			Look into the [available methods](https://core.telegram.org/bots/api#available-methods) section
+
+				* method (str): String with the desired method to be executed by the bot
+				* data (json): json with the data required by the method
+
+		### Return:
+				* Response: HTTP Response from the bot
+
+	"""
 	return requests.post(f'https://api.telegram.org/bot{os.getenv("TOKEN_TELEGRAM")}/'+method, data = data)
 
 def witRequest(texto):
+	"""
+		## witRequest
+
+		### Description:
+			Call the [Wit.ai](https://wit.ai/) API to process the message received by the bot
+
+		### Parameters:
+				* texto (str): String with the message to be processed
+
+		### Return:
+				* Response: HTTP Response with the processed information
+
+	"""
 	header = {'Authorization': os.getenv('TOKEN_WIT')}
 	url_wit = 'https://api.wit.ai/message?v=20220128&q=' + requests.utils.requote_uri(texto)
 	
 	return requests.get(url_wit, headers=header)
 
+def __get_open_weather(location, unit):
+	"""
+		## __get_open_weather
+
+		### Description:
+			Call the [Open Weather](https://openweathermap.org/api) API to get the current weather
+
+		### Parameters:
+				* location (str): String with the desired weather location (City, country)
+				* unit (str): String with the units system wanted (could be: metric (Celsius), standard (Kelvin) or imperial (Fahrenheit))
+
+		### Return:
+				* Response: HTTP Response with the weather information
+
+	"""
+	url_ow = f'https://api.openweathermap.org/data/2.5/weather?q={location}&units={unit}&appid={os.getenv("TOKEN_OPENWEATHER")}'
+	
+	return requests.get(url_ow)
+
 def get_weather(location, unit='metric'):
-	unidad_d = dict(metric='Celsius', imperial='Fahrenheit', standard='Kelvin')
+	"""
+		## get_weather
+
+		### Description:
+			Gets the weather and return it in a string ready to be sent by the bot
+
+		### Parameters:
+				* location (str): String with the desired weather location (City, country)
+				* unit (str): String with the units system wanted (could be: metric (Celsius), standard (Kelvin) or imperial (Fahrenheit))
+
+		### Return:
+				* str: String with the weather information already formatted and ready to be sent by the bot
+
+	"""
+	unidad_d = dict(metric='ºC', imperial='ºF', standard='K')
 	icons_d = dict([(2, '⛈'), (3, '🌦'), (5, '🌧'), (6, '🌨'), (7, '🌫'), (800, '☀️'), (8, '☁️')])
 
-	response = get_open_weather(location, unit).json()
+	response = __get_open_weather(location, unit).json()
 	
 	if response["weather"][0]["id"] == 800:
 		icon = icons_d[800]
@@ -27,9 +89,7 @@ def get_weather(location, unit='metric'):
 	return f"*The weather in {str(location).capitalize()}: {icon} {response['weather'][0]['main']} & {int(response['main']['temp'])} {unidad_d[unit]}*"
 
 def set_weather(location, temp):
+	"""
+		### This function was only to try if the Wit.ai API was recognizig the intent well
+	"""
 	return f"Temperature of {str(location).capitalize()} set to {int(temp['valor'])} {temp['unidad']}"
-
-def get_open_weather(location, unit):
-	url_ow = f'https://api.openweathermap.org/data/2.5/weather?q={location}&units={unit}&appid={os.getenv("TOKEN_OPENWEATHER")}'
-	
-	return requests.get(url_ow)
